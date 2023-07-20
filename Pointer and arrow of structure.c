781256ed4a89708cc7c9360414c06e2d7f5edff9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-20 16:55:47 +0600

    Update Pointer and arrow of structure.c

--- Pointer and arrow of structure.c
+++ Pointer and arrow of structure.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-struct Student
+struct student
 {
-    char name[50];
-    int roll;
-    float cgpa;
+    char name[100];
+    int id;
 };
 
 int main()
 {
-    struct Student s1 = {"Emon", 5601, 3.90};
-    printf("Name: %s\n", s1.name);
-    printf("Roll: %d\n", s1.roll);
-    printf("CGPA: %.2f\n", s1.cgpa);
-    struct Student *ptr;
-    ptr = &s1;
-    printf("Address of s1 = %x\n", ptr);
-    printf("Name of s1 with pointer = %s\n", (*ptr).name);
-    printf("Roll of s1 with arrow = %d\n", ptr->roll);
+    struct student s1;
+    strcpy(s1.name, "Emon");
+    s1.id = 5601;
+    struct student *ptr = &s1;
+    printf("Name: %s\n", (*ptr).name);
+    printf("ID = %d\n", ptr->id);
+    printf("Address simply = %x\n", &s1);
+    printf("Address using pointer = %x\n", ptr);
     return 0;
 }
