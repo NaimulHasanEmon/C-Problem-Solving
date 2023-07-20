a13e240a256f212bafc8948b03d48d1a41249fc5
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-20 09:56:26 +0600

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+struct Student
+{
+    char name[50];
+    int roll;
+    float cgpa;
+};
+
+int main()
+{
+    struct Student s1 = {"Emon", 5601, 3.90};
+    printf("Name: %s\n", s1.name);
+    printf("Roll: %d\n", s1.roll);
+    printf("CGPA: %.2f\n", s1.cgpa);
+    struct Student *ptr;
+    ptr = &s1;
+    printf("Address of s1 = %x\n", ptr);
+    printf("Roll of s1 with pointer = %d\n", (*ptr).roll);
+    return 0;
+}
