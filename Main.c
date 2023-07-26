7d95bf85a029eabc23f1d4d631343bde255b967a
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-26 09:25:01 +0600

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d", ptr->data);
+        ptr = ptr->next;
+        if (ptr != NULL)
+        {
+            printf(" -> ");
+        }
+    }
+}
+int main()
+{
+    struct Node *head, *second, *third, *fourth;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+    head->data = 7;
+    head->next = second;
+    second->data = 45;
+    second->next = third;
+    third->data = 69;
+    third->next = fourth;
+    fourth->data = 100;
+    fourth->next = NULL;
+    LinkedListTraversal(head);
+    return 0;
+}
